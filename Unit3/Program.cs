3e59a210233de5e84dc73421f7a4c5e2892224b7
ProkopenkoIA <boroda5384@mail.ru>
2023-01-29 19:56:56 +0500

    коммит третьего задания

---
+++ Unit3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+double A;
+Console.WriteLine("Введите значение А");
+
+A = Convert.ToDouble(Console.ReadLine());
+
+if (A % 2 == 0)
+{
+    Console.WriteLine(" Значение А - четное!");
+}
+else
+{
+   Console.WriteLine(" Значение А - не четное!"); 
+}
