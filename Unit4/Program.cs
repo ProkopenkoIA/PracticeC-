db6b0302465eac75827d18cfd8e36762c62baa08
ProkopenkoIA <boroda5384@mail.ru>
2023-01-29 20:24:19 +0500

    коммит чертвертого задания

---
+++ Unit4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Clear();
+
+double A;
+string S = "";
+
+Console.Write("Введите значение А ");
+
+A = Convert.ToDouble(Console.ReadLine());
+
+
+for (int i = 1; i<Convert.ToInt32(A); i++)
+{
+    if (i % 2 == 0)
+    {
+       S = S + Convert.ToString(i)+" "; 
+    }
+
+}
+
+Console.WriteLine(" Все четные от 1 до "+Convert.ToString(A)+": "+S);
