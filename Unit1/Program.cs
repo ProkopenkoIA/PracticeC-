b97be59dfbb6c28b227114db5d4ebcb03da13553
ProkopenkoIA <boroda5384@mail.ru>
2023-01-29 19:37:19 +0500

    коммит первого задания

---
+++ Unit1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int A, B;
+Console.WriteLine("Введите значение А");
+
+A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение B");
+
+B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("A = "+ Convert.ToString(A));
+Console.WriteLine("B = "+ Convert.ToString(B));
+
+if (A>B)
+{
+    Console.WriteLine(" Результат: A больше B");
+}
+else
+{
+    if (A!=B)
+    {
+        Console.WriteLine(" Результат: B больше A");
+    } 
+    else
+        Console.WriteLine(" Числа равны");
+}
+
