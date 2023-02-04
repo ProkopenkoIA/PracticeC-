3fd61805adc7b7a94c5d64a0e26ff8c30432dc98
ProkopenkoIA <boroda5384@mail.ru>
2023-02-05 02:05:10 +0500

    Урое 2: Задание 3

---
+++ Lsn2_Unit3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int A = new Random().Next(1,8);
+
+Console.WriteLine("Номер дня недели = "+Convert.ToString(A));
+
+if (A>6)
+    {Console.WriteLine("Выходной");}
+else 
+    {Console.WriteLine("Не выходной");}
