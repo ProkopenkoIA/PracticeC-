f521c734b5cf774493aee2318c6d99282ee7eb41
ProkopenkoIA <boroda5384@mail.ru>
2023-02-05 00:58:33 +0500

    Урое 2: Задание 1

---
+++ Lsn2_Unit1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int A = new Random().Next(100,1000), B;
+
+
+Console.WriteLine("Значение А = "+Convert.ToString(A));
+
+B = (A%100)/10;
+
+Console.WriteLine("Вторая цифра = "+Convert.ToString(B));
