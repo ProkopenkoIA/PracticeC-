0184bee509fdc2c11f7e8737757853a260bda10d
ProkopenkoIA <boroda5384@mail.ru>
2023-02-05 02:39:07 +0500

    Урое 2: доп задание

---
+++ Lsn2_Unit4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int A = Convert.ToInt32(Console.ReadLine()), MaxValue, SecondMaxValue;
+
+MaxValue = A;
+SecondMaxValue = 0;
+
+while (A!=0)
+{
+    A = Convert.ToInt32(Console.ReadLine());
+    //-- Сначала установим новым максимум
+    if (A>MaxValue)
+    {
+        SecondMaxValue = MaxValue;
+        MaxValue = A;
+    }
+    //-- Потом проверим второй максимум
+    //-- похоже на не совсем оптимальное решение, но эта проверка необходима, например в случае 8 15 99 73 16 2 0
+    else 
+    {
+        if (SecondMaxValue<A)
+        {
+            SecondMaxValue = A;     
+        }
+    }
+        
+}
+
+Console.WriteLine("Ответ: "+Convert.ToString(SecondMaxValue));
+
