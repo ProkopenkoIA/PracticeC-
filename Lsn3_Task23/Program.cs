23dfe9b01a0c0f5dfd84110caa4295234d41fa0b
ProkopenkoIA <boroda5384@mail.ru>
2023-02-09 22:26:25 +0500

    Задание №23

---
+++ Lsn3_Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+
+int A, i;
+
+Console.Write("Введите значение А =");
+A = Convert.ToInt32(Console.ReadLine());
+
+for (i=1;i<=A;i++)
+ {Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");}
