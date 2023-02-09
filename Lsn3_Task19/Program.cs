1dd107c3a1fb65d8646fcdabb34cdafdda70d88d
ProkopenkoIA <boroda5384@mail.ru>
2023-02-09 23:10:10 +0500

    Задание №19

---
+++ Lsn3_Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+string str1, str2;
+int i, len;
+
+Console.Write("Введите число ");
+
+str1 = Console.ReadLine();
+str2 = "";
+
+len = str1.Length-1;
+
+for (i = len;i>=0; i--)
+{
+  str2 = str2 + str1[i];  
+}
+
+if (str1==str2)
+{Console.WriteLine("Число является палиндромом");}
+else
+{Console.WriteLine("Число не является палиндромом");}
