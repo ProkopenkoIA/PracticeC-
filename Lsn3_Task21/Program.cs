5749f893555614cbd26b0e7abccc2c198477b20a
ProkopenkoIA <boroda5384@mail.ru>
2023-02-09 22:44:32 +0500

    Задание №21

---
+++ Lsn3_Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+double x1, x2, y1, y2, z1, z2, result;
+
+Console.Write("Введите значение x1 = ");
+
+x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение y1 = ");
+
+y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение z1 = ");
+
+z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение x2 = ");
+x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение y2 = ");
+y2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение z2 = ");
+z2 = Convert.ToDouble(Console.ReadLine());
+
+result = Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)),2);
+
+Console.WriteLine($"Расстояние между точками = {result}");
