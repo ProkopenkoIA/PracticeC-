aa953ac04bb325407d2b7925fe4fc82ccf3fe8c8
ProkopenkoIA <boroda5384@mail.ru>
2023-02-12 00:53:35 +0500

    Урок 4: задание 27

---
+++ Lesson4/Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int Sum (int a)
+{
+    string s, str;
+    int  i, result;
+
+    result = 0;
+
+    str = Convert.ToString(a);
+
+    //Console.Write($"{str.Length-1}"); 
+
+    for (i = 0; i<=str.Length-1; i++)
+    {
+        s =  Convert.ToString(str[i]);  
+        result = result +  Convert.ToInt32(s);
+    }
+    return result;
+
+}
+
+
+int a;
+
+Console.Write("Введите значение A = ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Результат {Sum(a)}");
+
