bd44d7419ee27b6ee45f92fbdefcac5798cb5f41
ProkopenkoIA <boroda5384@mail.ru>
2023-02-12 00:33:04 +0500

    Урок 4: задание 25

---
+++ Lesson4/Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int Pow (int a, int b)
+{
+    int c, i;
+
+    c = 1;
+
+    for (i = 1; i<=b; i++)
+    {
+        c = c * a;
+    }
+    return c;
+
+}
+
+int a, b;
+
+Console.Write("Введите значение A = ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение B = ");
+
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Результат {Pow(a,b)}");
+
