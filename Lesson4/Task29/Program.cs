ce85bce33a157099236b27e00935cffd4d4204df
ProkopenkoIA <boroda5384@mail.ru>
2023-02-12 02:11:33 +0500

    Урок 4: задание 29

---
+++ Lesson4/Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+int a, i, b;
+
+Console.Write("Введите количество учеников = ");
+a = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [a];
+
+for (i=0; i<=a-1; i++)
+{   
+   b =  new Random().Next(160,201);
+   array[i] = b;
+}
+
+Array.Sort(array);
+Array.Reverse(array);
+Console.WriteLine($"{string.Join(" ,",array)}");
+
+Console.Write("Введите рост Пети = ");
+b = Convert.ToInt32(Console.ReadLine());
+
+for (i=0; i<=a-1; i++)
+{  
+    if (b>array[i])
+    {
+        Console.WriteLine($" Позиция Пети = {i+1}");     
+        return; 
+    }
+}
+
+Console.WriteLine($" Позиция Пети = {i+1}"); 
+
