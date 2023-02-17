ecf2b2a08b7d9e0e763c57a3edd01c77b8b8d1df
ProkopenkoIA <boroda5384@mail.ru>
2023-02-17 09:16:21 +0500

    Урок 5: задание 36 сумма элементов на не четных позициях

---
+++ Lesson5/Task36/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int answer (int [] a)
+{
+    int i,b;
+    b = 0;
+
+    for (i=0; i<=a.Length-1; i++)
+    {   
+        if (i % 2 == 1)
+        {
+         b = b + a[i];
+        }    
+    
+    }
+    return b;
+
+}
+
+void new_array (int [] a)
+{
+    int i,b;
+    for (i=0; i<=a.Length-1; i++)
+    {   
+    b =  new Random().Next(-10, 11);
+    a[i] = b;
+    }
+}
+
+int a, result;
+
+Console.Write("Задайте длину массива = ");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [a];
+
+new_array(array);
+
+Console.WriteLine($"{string.Join(" ,",array)}");
+
+result = answer(array);
+Console.Write($"Сумма элементов на не четных похициях = {result}");
