623aa17d79106d26e2c9791c444bb6b3b2cb65e9
ProkopenkoIA <boroda5384@mail.ru>
2023-02-21 08:19:58 +0500

    Урок 6: задача 41, поиск числе больше 0 в массиве

---
+++ Lesson6/Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+Console.Clear();
+
+int calc (int [] arr)
+{
+    int i, b=0;  
+
+    for(i=0; i<=arr.Length-1; i++)
+    {
+        if (arr[i]>0)
+        {
+            b++;
+        }
+    }
+
+    return b;
+}
+
+Console.WriteLine("Введите строку чисел через пробел...");
+int [] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+
+Console.WriteLine($"[{string.Join(", ",numbers)}]");
+
+Console.WriteLine($"Числе болше ноля - {calc(numbers)}");
